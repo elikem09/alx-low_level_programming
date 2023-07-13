96f5a7f69888003deea85fafc3e6d0d5ca089e5c
elikem09 <worklatsi09@gmail.com>
2023-07-13 12:23:14 -0700

    add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,6 @@
 #include<unistd.h>
 int main(void)
 {
-fput("and that please of art is useful\" - Dora Korpar, 2015, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
