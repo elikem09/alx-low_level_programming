e6be3e52a043c4818e436cf3666049c8abe50c94
elikem09 <worklatsi09@gmail.com>
2023-07-14 06:56:11 -0700

    alphaBETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,18 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main() {
-    char letter;
+int main(void)
+{
+char letter;
 
-    for (letter = 'A'; letter <= 'Z'; letter++) {
-        putchar(letter);
-    }
-
-    for (letter = 'a'; letter <= 'z'; letter++) {
-        putchar(letter);
-    }
-
-    return 0;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+for (letter = 'A'; letter <= 'Z'; letter++)
+{
+putchar(letter);
+}
+return (0);
 }
 
