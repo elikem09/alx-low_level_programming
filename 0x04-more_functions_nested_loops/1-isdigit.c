21c3b0d64f894db2f9df20f4e588fb35089da0f7
elikem09 <worklatsi09@gmail.com>
2023-07-20 21:48:02 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for lowercase character
+ * @c: the character to be checked
+ * Return: 1 for uppercase or 0 for anything else.
+ */
+
+int _isdigit(int c)
+
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
